47559dc311c3c0c860e15d3205e4ba3adf8fa5fd
KeiKun <the.keikun@gmail.com>
2021-02-12 21:46:07 +0800

    Refactored method of copying skills via Plagiarism and Reproduce. Separated Plagiarized and Reproduced skill list.

--- src/map/skill.h
+++ src/map/skill.h
@@ -139,6 +139,7 @@ enum e_skill_inf2 {
 	INF2_IS_COMBO_SKILL    = 0x100000, // Sets whether a skill can be used in combos or not
 	INF2_NO_STASIS         = 0x200000,
 	INF2_NO_KAGEHUMI       = 0x400000,
+	INF2_ALLOW_PLAGIARIZE  = 0x800000,
 };
 
 
